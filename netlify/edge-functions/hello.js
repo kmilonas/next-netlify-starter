1243a34d41c009a3a7b746708f5dd3d9d15bc3f7
kmilonas <32306373+kmilonas@users.noreply.github.com>
2023-10-08 14:37:21 -0700

    Update hello.js

--- netlify/edge-functions/hello.js
+++ netlify/edge-functions/hello.js
@@ -1,3 +1,6 @@
-export default () => new Response("Hello world");
+
+import { Context } from "@netlify/edge-functions";
+
+export default () => new Response("Hello world in ?{context.geo?.country?.code}");
 
 export const config = { path: "/test" };

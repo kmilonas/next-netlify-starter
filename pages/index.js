8bb29afe8772c3fc8e909836e0921804cfbcd106
kmilonas <32306373+kmilonas@users.noreply.github.com>
2023-10-08 11:33:30 -0700

    try country

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import type { Config, Context } from "@netlify/edge-functions";
+
 
 export default function Home() {
   return (
@@ -13,8 +15,9 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app<3!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+          Get started by editing <code>pages/index.js. </code>
+	You are in {context.geo.country?.name}
+	</p>
       </main>
 
       <Footer />

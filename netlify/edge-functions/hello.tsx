3e5775d8618b4702ecd4b0df785fafbe282ed1ad
kmilonas <32306373+kmilonas@users.noreply.github.com>
2023-10-21 14:05:49 -0700

    Update hello.tsx

--- netlify/edge-functions/hello.tsx
+++ netlify/edge-functions/hello.tsx
@@ -3,14 +3,11 @@ import { renderToReadableStream } from "https://esm.sh/react-dom/server";
 import type { Config, Context } from "@netlify/edge-functions";
 
 export default async function handler(req: Request, context: Context) {
-  const mylink0 = "//umap.openstreetmap.fr";
-  const mylink1 = "/en/map/swim-spots_955902?scaleControl=false&miniMap=false&scrollWheelZoom=false&zoomControl=true&allowEdit=false&moreControl=false&searchControl=null&tilelayersControl=null&embedControl=null&datalayersControl=true&onLoadPanel=undefined&captionBar=false&captionMenus=true";
-  const mylink = mylink0 + mylink1;
+  const mylink0 = "//umap.openstreetmap.fr/en/map/swim-spots_955902?scaleControl=false&miniMap=false&scrollWheelZoom=false&zoomControl=true&editMode=disabled&moreControl=true&searchControl=null&tilelayersControl=null&embedControl=null&datalayersControl=false&onLoadPanel=undefined&captionBar=false&captionMenus=true#7/";
+  const mylink = mylink0 + context.geo.latitude + "/" + context.geo.longitude;
   const stream = await renderToReadableStream(
     <html>
-      <title>Hello</title>
       <body>
-        <h1>Hello {context.geo.country?.name}</h1>
 
         <iframe width="100%" height="300px" frameborder="0" allowfullscreen allow="geolocation" src= {mylink}></iframe>
       </body>

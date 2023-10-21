c8d5fd6bae7cb19de3ca49d053d5d02585faa1dc
kmilonas <32306373+kmilonas@users.noreply.github.com>
2023-10-21 11:52:39 -0700

    Update hello.tsx

--- netlify/edge-functions/hello.tsx
+++ netlify/edge-functions/hello.tsx
@@ -6,8 +6,9 @@ export default async function handler(req: Request, context: Context) {
   const stream = await renderToReadableStream(
     <html>
       <body>
-<iframe width="100%" height="300px" frameborder="0" allowfullscreen allow="geolocation" src="//umap.openstreetmap.fr/en/map/swim-spots_955902?scaleControl=false&miniMap=false&scrollWheelZoom=false&zoomControl=true&editMode=disabled&moreControl=true&searchControl=null&tilelayersControl=null&embedControl=null&datalayersControl=true&onLoadPanel=undefined&captionBar=false&captionMenus=true#7/58.119/18.787"></iframe><p><a href="//umap.openstreetmap.fr/en/map/swim-spots_955902?scaleControl=false&miniMap=false&scrollWheelZoom=true&zoomControl=true&editMode=disabled&moreControl=true&searchControl=null&tilelayersControl=null&embedControl=null&datalayersControl=true&onLoadPanel=undefined&captionBar=false&captionMenus=true#7/{context.geo.latitude}/{context.geo.longitude}<">See full screen</a></p>
-</body>
+<iframe width="100%" height="300px" frameborder="0" allowfullscreen allow="geolocation" 
+src="//umap.openstreetmap.fr/en/map/swim-spots_955902?scaleControl=false&miniMap=false&scrollWheelZoom=false&zoomControl=true&editMode=disabled&moreControl=true&searchControl=null&tilelayersControl=null&embedControl=null&datalayersControl=true&onLoadPanel=undefined&captionBar=false&captionMenus=true#7/58.119/18.787"></iframe><p><a href="//umap.openstreetmap.fr/en/map/swim-spots_955902?scaleControl=false&miniMap=false&scrollWheelZoom=true&zoomControl=true&editMode=disabled&moreControl=true&searchControl=null&tilelayersControl=null&embedControl=null&datalayersControl=false&onLoadPanel=undefined&captionBar=false&captionMenus=true#7/"{context.geo.latitude}/{context.geo.longitude}>
+  </p></body>
     </html>
   );
 

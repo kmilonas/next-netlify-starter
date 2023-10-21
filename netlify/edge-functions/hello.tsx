67c7cdef42b3e4ad92024cbd82eff639c278834c
kmilonas <32306373+kmilonas@users.noreply.github.com>
2023-10-21 13:11:23 -0700

    Update hello.tsx

--- netlify/edge-functions/hello.tsx
+++ netlify/edge-functions/hello.tsx
@@ -3,13 +3,14 @@ import { renderToReadableStream } from "https://esm.sh/react-dom/server";
 import type { Config, Context } from "@netlify/edge-functions";
 
 export default async function handler(req: Request, context: Context) {
+  global mylink = "//umap.openstreetmap.fr/en/map/swim-spots_955902?scaleControl=false&miniMap=false&scrollWheelZoom=false&zoomControl=true&allowEdit=false&moreControl=false&searchControl=null&tilelayersControl=null&embedControl=null&datalayersControl=true&onLoadPanel=undefined&captionBar=false&captionMenus=true"
   const stream = await renderToReadableStream(
     <html>
       <title>Hello</title>
       <body>
         <h1>Hello {context.geo.country?.name}</h1>
 
-        <iframe width="100%" height="300px" frameborder="0" allowfullscreen allow="geolocation" src="//umap.openstreetmap.fr/en/map/swim-spots_955902?scaleControl=false&miniMap=false&scrollWheelZoom=false&zoomControl=true&allowEdit=false&moreControl=false&searchControl=null&tilelayersControl=null&embedControl=null&datalayersControl=true&onLoadPanel=undefined&captionBar=false&captionMenus=true"></iframe>
+        <iframe width="100%" height="300px" frameborder="0" allowfullscreen allow="geolocation" src= {mylink}></iframe>
       </body>
     </html>
   );

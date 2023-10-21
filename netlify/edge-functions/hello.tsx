3be493bc313be5cf03933d8d778965b5ca0960b6
kmilonas <32306373+kmilonas@users.noreply.github.com>
2023-10-21 12:56:37 -0700

    Update hello.tsx

--- netlify/edge-functions/hello.tsx
+++ netlify/edge-functions/hello.tsx
@@ -3,13 +3,13 @@ import { renderToReadableStream } from "https://esm.sh/react-dom/server";
 import type { Config, Context } from "@netlify/edge-functions";
 
 export default async function handler(req: Request, context: Context) {
-  let a0 = "http://"
-  let a1 = 'www.google.com'
+  //let a0 = "http://"
+  //let a1 = 'www.google.com'
   const stream = await renderToReadableStream(
     <html>
       <body>
 <iframe width="100%" height="300px" frameborder="0"  
-src={a0}{a1}></iframe>
+src="http://www.google.com"></iframe>
  </body>
     </html>
   );

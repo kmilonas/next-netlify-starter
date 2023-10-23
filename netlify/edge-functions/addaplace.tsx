a51447d6129307cf6e40597d25948815ee835e33
kmilonas <32306373+kmilonas@users.noreply.github.com>
2023-10-23 05:55:07 -0700

    Update addaplace.tsx

--- netlify/edge-functions/addaplace.tsx
+++ netlify/edge-functions/addaplace.tsx
@@ -6,7 +6,7 @@ export default async function handler(req: Request, context: Context) {
   const mylink0 = "https://umap.openstreetmap.fr/en/map/swim-spots_875791#"
   const mylink = mylink0 + context.geo.latitude + "/" + context.geo.longitude;
   const mystyle = "body{font-family: sans-serif; } iframe{height: 60%;}"
-  //const myiframestyle = "{position: absolute; height: 60%; border: none}"
+  const myiframestyle = "{height: 60%}"
   const stream = await renderToReadableStream(
     <html>
       <head>
@@ -28,7 +28,7 @@ export default async function handler(req: Request, context: Context) {
   <li>Put the pin at the location of your plunge spot. Tell the world how amazing it is and how to get there :)
 </li>
 </ol>
-        <iframe width="80%" frameborder="0" allowfullscreen allow="geolocation" src= {mylink}></iframe>
+        <iframe width="80%" frameborder="0" style = {myiframestyle} allowfullscreen allow="geolocation" src= {mylink}></iframe>
         
 
       </body>

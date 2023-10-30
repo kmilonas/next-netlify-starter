bbc2da91ac6c450276e569766bf66ca6d2bd2984
kmilonas <32306373+kmilonas@users.noreply.github.com>
2023-10-30 07:23:10 -0700

    Update hello.tsx

--- netlify/edge-functions/hello.tsx
+++ netlify/edge-functions/hello.tsx
@@ -24,6 +24,7 @@ export default async function handler(req: Request, context: Context) {
     right: 0;
     width: 100%;
     height: 100%;
+    display: block;
   }`
   const stream = await renderToReadableStream(
     <html>

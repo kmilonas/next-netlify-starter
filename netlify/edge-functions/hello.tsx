1876fa07ca4980d93eceeeb35c3a6e319c0c66b5
kmilonas <32306373+kmilonas@users.noreply.github.com>
2023-10-30 18:40:04 -0700

    Update hello.tsx

--- netlify/edge-functions/hello.tsx
+++ netlify/edge-functions/hello.tsx
@@ -27,35 +27,8 @@ export default async function handler(req: Request, context: Context) {
     display: block;
   }`
 
-  const h2 = "import React from "https://esm.sh/react";
-import { renderToReadableStream } from "https://esm.sh/react-dom/server";
-import type { Config, Context } from "@netlify/edge-functions";
+  const h2 = "Guide to cold plunge spots, powered by you and the community";
 
-export default async function handler(req: Request, context: Context) {
-  const mylink0 = "//umap.openstreetmap.fr/en/map/swim-spots_955902?scaleControl=false&miniMap=false&scrollWheelZoom=false&zoomControl=true&editMode=disabled&moreControl=true&searchControl=null&tilelayersControl=null&embedControl=null&datalayersControl=false&onLoadPanel=undefined&captionBar=false&captionMenus=true#7/";
-  const mylink = mylink0 + context.geo.latitude + "/" + context.geo.longitude;
-  const mystyle =  `
-  body{font-family: sans-serif; } 
-  iframe{height: 60vh; width: 60vw;} 
-  .container {
-    position: relative;
-    overflow: hidden;
-    width: 75%;
-    padding-top: 56.25%; /* 16:9 Aspect Ratio (divide 9 by 16 = 0.5625) */
-  }
-  
-  /* Then style the iframe to fit in the container div with full height and width */
-  .responsive-iframe {
-    position: absolute;
-    top: 0;
-    left: 0;
-    bottom: 0;
-    right: 0;
-    width: 100%;
-    height: 100%;
-    display: block;
-  }`
-  const2 h2 = "Guide to cold plunge spots, powered by you and the community"
   const addaplace = `
   <p>
   Is your favorite spot missing? 
